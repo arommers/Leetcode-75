626335f9a5de748ed07a86784b9b89d4f43e961e
arommers <a3.p.rommers@gmail.com>
2024-05-27 16:31:27 +0200

    solved product of Array except selve

---
+++ productOfArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+    public:
+        std::vector<int> productExceptSelf(std::vector<int> &nums)
+        {
+            int n = nums.size();
+            std::vector<int> result(n, 1);
+
+            int prefix = 1;
+            for (int i = 0; i < n; ++i)
+            {
+                result[i] *= prefix;
+                prefix *= nums[i];
+            }
+
+            int suffix = 1;
+            for(int i = n - 1; i >= 0; --i)
+            {
+                result[i] *= suffix;
+                suffix *= nums[i];
+            }
+            return result;
+        }
+};
