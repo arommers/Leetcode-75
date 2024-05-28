eb9f2c03d3d6df380ba69f43cc46e30e4112df27
arommers <arommers@student.codam.nl>
2024-05-28 15:27:25 +0200

    started sliding window.05

--- sliding_window/max_average.cpp
+++ sliding_window/max_average.cpp
@@ -9,13 +9,13 @@ class Solution {
             for(int i = 0; i < k; ++i)
                 currentSum += nums[i];
             
-            double maxAverage = currentSum / k;
+            double maxAverage = currentSum;
 
             for (int i = k; i < nums.size(); ++i)
             {
                 currentSum += nums[i] - nums[i - k];
-                maxAverage = std::max(maxAverage, currentSum / k);
+                maxAverage = std::max(maxAverage, currentSum);
             }
-            return maxAverage;
+            return maxAverage / k;
         }
 };
