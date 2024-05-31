a90846553c0caba81e5b1545e2b190e714bd5bc3
arommers <a3.p.rommers@gmail.com>
2024-05-31 09:31:48 +0200

    changed hashmap folder

--- hashmaps/array_diffs.cpp
+++ hashmaps/array_diffs.cpp
@@ -7,11 +7,12 @@ class Solution {
     public:
         vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) 
         {
-            unordered_set<int> set1(nums1.begin(), nums1.end());
-            unordered_set<int> set2(nums2.begin(), nums2.end());
             vector<int> res1;
             vector<int> res2;
             vector<vector<int>> result;
+            
+            unordered_set<int> set1(nums1.begin(), nums1.end());
+            unordered_set<int> set2(nums2.begin(), nums2.end());
 
             for (int value : set1)
             {
