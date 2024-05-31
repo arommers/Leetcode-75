d1bd449941714919d3adeddecee6a9fa20efad56
arommers <a3.p.rommers@gmail.com>
2024-05-31 09:29:39 +0200

    started hashmaps

---
+++ hashmaps/array_diffs.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+    public:
+        vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) 
+        {
+            unordered_set<int> set1(nums1.begin(), nums1.end());
+            unordered_set<int> set2(nums2.begin(), nums2.end());
+            vector<int> res1;
+            vector<int> res2;
+            vector<vector<int>> result;
+
+            for (int value : set1)
+            {
+                if (set2.find(value) == set2.end())
+                    res1.push_back(value);
+            }
+
+            for (int value : set2)
+            {
+                if (set1.find(value) == set1.end())
+                    res2.push_back(value);
+            }
+            result.push_back(res1);
+            result.push_back(res2);
+            return result;
+        }
+};
